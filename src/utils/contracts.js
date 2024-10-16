211c10f8a71dd74e4af876b107ed7e3c52fe2ae1
Vivek Sahu <viveksahu1762@gmail.com>
2024-10-16 09:35:20 +0530

    Update contracts.js

--- src/utils/contracts.js
+++ src/utils/contracts.js
@@ -1,9 +1,9 @@
 import vestingABI from "@/abi/vesting.json";
 export const VESTINGABI = vestingABI;
 export const VESTING_CONTRACT_ADDRESS =
-  "0x6167696C08eD5C691a813E6bcA12Ea1a43bD3511";
+  "0x0A88A5D1f53cf07E93F93c94aB67Df38d0f3dCA9";
 
 import encyptedERC20ABI from "@/abi/encryptedERC20.json";
 export const ENCRYPTEDERC20ABI = encyptedERC20ABI;
 export const ERC20_CONTRACT_ADDRESS =
-  "0x19cb8892655AA9009d1dE8Fa4B96E131Da879f31";
+  "0x28FD2e1563Eac289DcE330eFCBd99Ff1D10166fA";
